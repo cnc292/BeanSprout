125d0c206e4e04a6af0c0d033d1b823e38528339
cnc292 <cnc292@nyu.edu>
2013-02-14 16:28:05 -0500

    initialcommit

---
+++ src/com/courtneyncoleman/beansprout/OtherActivity.java
@@ -0,0 +1,14 @@
+package com.courtneyncoleman.beansprout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OtherActivity extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.other_activity);
+		
+		
+		
+	}
+}
